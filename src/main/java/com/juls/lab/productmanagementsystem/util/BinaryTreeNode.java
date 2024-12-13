a2c18b60d572c9febf9f38ad11c685565e087c5a
adjetey-sowah <julius.sowah@amalitech.com>
2024-12-13 17:05:35 +0000

    Feature Implementation: Implemented the BinaryTree and operations for the categorization of product

---
+++ src/main/java/com/juls/lab/productmanagementsystem/util/BinaryTreeNode.java
@@ -0,0 +1,22 @@
+package com.juls.lab.productmanagementsystem.util;
+
+import com.juls.lab.productmanagementsystem.model.Category;
+import com.juls.lab.productmanagementsystem.model.Product;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class BinaryTreeNode {
+
+    private Category category;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+    private List<Product> products;
+
+    public BinaryTreeNode(Category category){
+        this.category = category;
+        this.products = new ArrayList<>();
+    }
+}
