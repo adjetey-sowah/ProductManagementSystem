09659105bfcfb0dee50def3ba1881d100798d12e
adjetey-sowah <julius.sowah@amalitech.com>
2024-12-15 11:21:37 +0000

    Implemented the add category method

--- src/main/java/com/juls/lab/productmanagementsystem/model/Category.java
+++ src/main/java/com/juls/lab/productmanagementsystem/model/Category.java
@@ -1,29 +1,51 @@
 package com.juls.lab.productmanagementsystem.model;
 
+import com.juls.lab.productmanagementsystem.model.Product;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId;
-    private String categoryName;
+    private Long id;
 
-    @ManyToOne
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
-    @OneToMany(mappedBy = "parent")
-    private Set<Category> subCategories;
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private Set<Category> subCategories = new HashSet<>();
 
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
