7980e51a24c4e8422e2ecfeb33e3a8d4d6a4bad9
adjetey-sowah <julius.sowah@amalitech.com>
2024-12-15 17:55:16 +0000

    Implemented the createCategory and getAllCategories endpoints

--- src/main/java/com/juls/lab/productmanagementsystem/controller/CategoryController.java
+++ src/main/java/com/juls/lab/productmanagementsystem/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package com.juls.lab.productmanagementsystem.controller;
 
+import com.juls.lab.productmanagementsystem.dto.CategoryBody;
 import com.juls.lab.productmanagementsystem.model.Category;
 import com.juls.lab.productmanagementsystem.service.CategoryService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,10 +19,15 @@ public class CategoryController {
     public final CategoryService categoryService;
 
     @GetMapping("/all")
-
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categoryList = this.categoryService.getAllCategories();
         return ResponseEntity.ok(categoryList);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody CategoryBody category){
+        Category newCategory = this.categoryService.createCategory(category.categoryName(), category.description(), category.parentId());
+        return ResponseEntity.ok().body(newCategory);
     }
 
 }
---
+++ src/main/java/com/juls/lab/productmanagementsystem/dto/CategoryBody.java
@@ -0,0 +1,4 @@
+package com.juls.lab.productmanagementsystem.dto;
+
+public record CategoryBody (String categoryName, String description, Long parentId){
+}
