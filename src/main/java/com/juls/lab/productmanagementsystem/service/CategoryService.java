8451eee38e03b0366dfb036d17b5c8956ac7a95d
adjetey-sowah <julius.sowah@amalitech.com>
2024-12-15 11:21:37 +0000

    Implemented the add category method

--- src/main/java/com/juls/lab/productmanagementsystem/service/CategoryService.java
+++ src/main/java/com/juls/lab/productmanagementsystem/service/CategoryService.java
@@ -1,17 +1,28 @@
 package com.juls.lab.productmanagementsystem.service;
 
 import com.juls.lab.productmanagementsystem.model.Category;
+import com.juls.lab.productmanagementsystem.model.Product;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
+@Service
+@Transactional
 public interface CategoryService {
 
-    Optional<Category> getCategoryById(Long id);
+    Category getCategoryById(Long id);
     List<Category> getAllCategories();
-    Category createCategory(Category category);
-    void deleteCategory(Long categoryId);
+    Category createCategory(String name, String description, Long parentId);
+
+    void deleteCategory(Long categoryId, Long reassignToCategoryId);
+
     Category updateCategory(Category category);
+    Set<Category> getSubCategories(Long categoryId);
+    List<Product> getProductsUnderCategory(Long categoryId);
+    void moveProducts(Long sourceCategoryId, Long targetCategoryId);
+    List<Category> getCategoryTree();
 
 
 }
--- src/main/java/com/juls/lab/productmanagementsystem/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/juls/lab/productmanagementsystem/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,24 @@
 package com.juls.lab.productmanagementsystem.service.impl;
 
 import com.juls.lab.productmanagementsystem.model.Category;
+import com.juls.lab.productmanagementsystem.model.Product;
+import com.juls.lab.productmanagementsystem.repository.CategoryRepository;
 import com.juls.lab.productmanagementsystem.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
+
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
+    private final CategoryRepository categoryRepository;
+
     @Override
-    public Optional<Category> getCategoryById(Long id) {
-        return Optional.empty();
+    public Category getCategoryById(Long id) {
+        return this.categoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Category not found with ID: " + id));
     }
 
     @Override
@@ -19,16 +27,46 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public Category createCategory(Category category) {
-        return null;
+    public Category createCategory(String name, String description, Long parentId) {
+        Category category = new Category();
+        category.setName(name);
+        category.setDescription(description);
+        if(parentId != null){
+            Category parent = this.categoryRepository.findById(parentId)
+                    .orElseThrow(() -> new IllegalArgumentException("Parent category not found"));
+            category.setParent(parent);
+            parent.getSubCategories().add(category);
+        }
+        return this.categoryRepository.save(category);
     }
     @Override
-    public void deleteCategory(Long categoryId) {
-
+    public void deleteCategory(Long categoryId, Long reassignToCategoryId) {
+        Category category = getCategoryById(reassignToCategoryId);
     }
 
     @Override
     public Category updateCategory(Category category) {
         return null;
+    }
+
+    @Override
+    public Set<Category> getSubCategories(Long categoryId) {
+        Category category = getCategoryById(categoryId);
+        return category.getSubCategories();
+    }
+
+    @Override
+    public List<Product> getProductsUnderCategory(Long categoryId) {
+        return List.of();
+    }
+
+    @Override
+    public void moveProducts(Long sourceCategoryId, Long targetCategoryId) {
+
+    }
+
+    @Override
+    public List<Category> getCategoryTree() {
+        return List.of();
     }
 }
