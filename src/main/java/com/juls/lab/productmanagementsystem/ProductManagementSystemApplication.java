38e2bc791135dcdfce23c653b11ec7c7b1af4c39
adjetey-sowah <julius.sowah@amalitech.com>
2024-12-18 12:35:39 +0000

    uitl: Implemented different profiles based on the environment

--- src/main/java/com/juls/lab/productmanagementsystem/ProductManagementSystemApplication.java
+++ src/main/java/com/juls/lab/productmanagementsystem/ProductManagementSystemApplication.java
@@ -1,5 +1,6 @@
 package com.juls.lab.productmanagementsystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductManagementSystemApplication {
 
     public static void main(String[] args) {
+
+        // Load .env file into the environment
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./") // Specify the directory of the .env file
+                .load();
+
+        // Optional: Print a variable to confirm it was loaded
+        System.out.println("MongoDB URL: " + dotenv.get("MONGO_DB_URL"));
+
         SpringApplication.run(ProductManagementSystemApplication.class, args);
+
     }
 
 }
